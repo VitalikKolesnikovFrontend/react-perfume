bb1b03fb9e280bbcaf164bbbd648fabc3966e27f
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2023-10-30 20:15:01 +0300

    Доделал полностью вёрстку

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2 className="mb-30">Корзина</h2>
+          <h2 className="d-flex justify-between mb-30">Корзина <img className="removeBtn cu-p" src="/img/remove.svg" alt="remove"/></h2>
+          
           <div className="items">
             <div className="cartItem d-flex align-center mb-20">
               <img className="mr-20" width={70} height={70} src="/img/1.jpg" alt="1"/>
@@ -31,18 +32,21 @@ function App() {
               <img className="removeBtn" src="/img/remove.svg" alt="remove"/>
             </div>
           </div>
-          <ul className="cartTotalBlock">
-            <li className="d-flex">
-              <span>Итого:</span>
-              <div></div>
-              <b>22050 руб.</b>
-            </li>
-            <li className="d-flex">
-              <span>Налог 5%</span>
-              <div></div>
-              <b>1070 руб.</b>
-            </li>
-          </ul>
+          <div className="cartTotalBlock">
+            <ul>
+              <li className="d-flex">
+                <span>Итого:</span>
+                <div></div>
+                <b>22050 руб.</b>
+              </li>
+              <li className="d-flex">
+                <span>Налог 5%</span>
+                <div></div>
+                <b>1070 руб.</b>
+              </li>
+            </ul>
+            <button className="greenButton">Оформить заказ <img src="/img/arrow2.svg" alt="arrow"/> </button>
+          </div>
         </div>
       </div>
       <header className="d-flex justify-between align-center">
